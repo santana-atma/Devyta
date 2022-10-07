a7c8e97492139f8c7e3e7862c7927be426d19caa
Santana <zeozaqy@gmail.com>
2022-10-07 15:58:49 +0700

    adding base project template

---
+++ API/Context/MyContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Context
+{
+    public class MyContext :DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> dbContext) : base(dbContext)
+        {
+
+        }
+        //public DbSet<Divisi> Divisi { get; set; } 
+    }
+}
---
+++ API/ViewModels/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.ViewModels
+{
+    public class Login
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
