cd5d31a1b237df67186341d215c5dd737432fdf8
Andhika Pramana <andhikapramana101@gmail.com>
2022-10-08 11:07:00 +0700

    Menambahkan ViewModel Perbaikan

---
+++ API/ViewModels/Perbaikan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.ViewModels
+{
+    public class Perbaikan
+    {
+        public int Barang_Id { get; set; }
+        public string Keterangan { get; set; }
+        public double Biaya { get; set; }
+        public int Jumlah { get; set; }
+        public string Status { get; set; }
+        public DateTime Tanggal_Terima { get; set; }
+        public DateTime Tanggal_Selesai { get; set; }
+    }
+}
