573e4ec4846b9ff55a06c2a421bd3c60069ca806
Santana <zeozaqy@gmail.com>
2022-10-08 17:07:45 +0700

    Revert "expanding viewmodel for responseLogin"
    This reverts commit 512bb1af486c8a2ae6cf03eede122e7c03411710.

--- API/ViewModels/ResponseLogin.cs
+++ API/ViewModels/ResponseLogin.cs
@@ -10,8 +10,6 @@ namespace API.ViewModels
         public int Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
-        public string Telp { get; set; }
-        public string Alamat { get; set; }
         public string Role { get; set; }
     }
 }
